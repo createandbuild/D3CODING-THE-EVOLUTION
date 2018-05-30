76efcfa0ccfa8838629a18db4bb69db22e60538b
createandbuild <33373263+createandbuild@users.noreply.github.com>
2018-05-30 12:14:54 -0400

    data visualization added

--- public/JS/components/facial-feature.js
+++ public/JS/components/facial-feature.js
@@ -2,14 +2,14 @@ var margin = {top: 20, bottom: 30, left: 40, right: 20},
     width = 1200 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-var movement = d3.select(".movement")
+var facialFeature = d3.select(".facial-feature")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("assets/data/movement.csv", function(error, data) {
+d3.csv("assets/data/facial-feature.csv", function(error, data) {
   data.forEach(function(d) {
       d.stage = +(d.stage);
       d.human = +(d.human);
@@ -32,35 +32,35 @@ d3.csv("assets/data/movement.csv", function(error, data) {
   x.domain(d3.extent(data, function(d) { return d.stage; }));
   y.domain([d3.min(data, function(d) { return Math.min(d.humanoid, d.human); }), d3.max(data, function(d) { return Math.max(d.humanoid, d.human); })]);
 
-  movement.append("path")
+  facialFeature.append("path")
       .data([data])
-      .attr("class", "line4a")
+      .attr("class", "line2a")
       .attr("d", valueline);
 
-  movement.append("path")
+  facialFeature.append("path")
       .data([data])
-      .attr("class", "line4b")
+      .attr("class", "line2b")
       .attr("d", valueline2);
 
-  movement.append("g")
+  facialFeature.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
-  movement.append("text")
+  facialFeature.append("text")
       .attr("transform", "translate(" + (width/2) + " ," + (height + margin.top) + ")")
       .attr("class", "label")
       .style("text-anchor", "middle")
       .text("Stages");
 
-  movement.append("g")
+  facialFeature.append("g")
       .call(d3.axisLeft(y));
 
-  movement.append("text")
+  facialFeature.append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 50 - margin.left)
       .attr("x",0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "label")
       .style("text-anchor", "middle")
-      .text("Movement");
+      .text("Cranial / Facial Features");
 });
--- public/JS/components/size.js
+++ public/JS/components/size.js
@@ -2,53 +2,65 @@ var margin = {top: 20, bottom: 30, left: 40, right: 20},
     width = 1200 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-var x = d3.scaleBand()
-          .range([0, width])
-          .padding(0.1);
-var y = d3.scaleLinear()
-          .range([height, 0]);
-
 var size = d3.select(".size")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 
 d3.csv("assets/data/size.csv", function(error, data) {
-  if(error) throw error;
   data.forEach(function(d) {
-    d.sales = +d.sales;
+      d.stage = +(d.stage);
+      d.human = +(d.human);
+      d.humanoid = +(d.humanoid);
   });
 
-  x.domain(data.map(function(d) { return d.salesperson; }));
-  y.domain([0, d3.max(data, function(d) { return d.sales; })]);
+  if (error) throw error;
 
-  size.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.salesperson); })
-      .attr("width", x.bandwidth())
-      .attr("y", function(d) { return y(d.sales); })
-      .attr("height", function(d) { return height - y(d.sales); });
+  var x = d3.scaleLinear().range([0, width]);
+  var y = d3.scaleLinear().range([height, 0]);
+
+  var valueline = d3.line()
+      .x(function(d) { return x(d.stage); })
+      .y(function(d) { return y(d.human); });
+
+  var valueline2 = d3.line()
+    .x(function(d) { return x(d.stage); })
+    .y(function(d) { return y(d.humanoid); });
+
+  x.domain(d3.extent(data, function(d) { return d.stage; }));
+  y.domain([d3.min(data, function(d) { return Math.min(d.humanoid, d.human); }), d3.max(data, function(d) { return Math.max(d.humanoid, d.human); })]);
+
+  size.append("path")
+      .data([data])
+      .attr("class", "line1a")
+      .attr("d", valueline);
+
+  size.append("path")
+      .data([data])
+      .attr("class", "line1b")
+      .attr("d", valueline2);
 
   size.append("g")
       .attr("transform", "translate(0," + height + ")")
       .call(d3.axisBottom(x));
 
-  // svg.append("text")
-  //     .attr("transform", "translate(" + (width/2) + " ," + (height + margin.top + 20) + ")")
-  //     .style("text-anchor", "middle")
-  //     .text("Height");
+  size.append("text")
+      .attr("transform", "translate(" + (width/2) + " ," + (height + margin.top) + ")")
+      .attr("class", "label")
+      .style("text-anchor", "middle")
+      .text("Stages");
 
   size.append("g")
       .call(d3.axisLeft(y));
-  //
-  // svg.append("text")
-  //       .attr("transform", "rotate(90)")
-  //       .attr("y", 0 - margin.left)
-  //       .attr("x",0 - (height / 2))
-  //       .attr("dy", "1em")
-  //       .style("text-anchor", "middle")
-  //       .text("Stage");
-})
+
+  size.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 50 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "label")
+      .style("text-anchor", "middle")
+      .text("Height (cm)");
+});
